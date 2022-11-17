1f1a3de55161d7eeadbe265bc1ee240acc9f7b62
Tasnim Mollah <tasnmmollah@gmail.com>
2022-11-17 13:05:47 -0500

    trying userImage query

--- screens/Profile.js
+++ screens/Profile.js
@@ -7,12 +7,14 @@ import {
   Image,
   SafeAreaView,
 } from "react-native";
-import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view"
-import React from "react";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import React, {useEffect} from "react";
 import { Icon, Input, Avatar, Divider } from "@rneui/themed";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
+import firebase from "firebase/compat";
+
 
 const fields = [
   { id: 1, field: "Email" },
@@ -34,7 +36,14 @@ const firstItem = () => {
   return (
     <View>
       <TouchableOpacity>
-        <Avatar rounded containerStyle={{alignSelf: "center", paddingBottom: 10}} size="large" source={{uri: "https://c8.alamy.com/comp/2HYCH09/larry-david-attending-the-natural-resources-defense-councils-stand-up!-event-at-the-wallis-annenberg-center-for-the-performing-arts-2HYCH09.jpg"}}/>
+        <Avatar
+          rounded
+          containerStyle={{ alignSelf: "center", paddingBottom: 10 }}
+          size="large"
+          source={{
+            uri: "https://c8.alamy.com/comp/2HYCH09/larry-david-attending-the-natural-resources-defense-councils-stand-up!-event-at-the-wallis-annenberg-center-for-the-performing-arts-2HYCH09.jpg",
+          }}
+        />
       </TouchableOpacity>
     </View>
   );
@@ -51,29 +60,54 @@ const lastItem = () => {
 };
 
 const Profile = () => {
-  return (
+  console.log("CHECK HERE FOR UID", auth.currentUser.uid)
+  useEffect(()=> {
+    const getUser = async () => {
+      const usersRef = db.collection("users");
+      //console.log(usersRef)
+      const user = await usersRef.where("email", "==", "petedavidson@snl.com").get();
+      //const user = await firebase.firestore().collection('users').doc("qn91knUBt6Ysypra66OAgBKJnWi1").get();
+      console.log("CHECK HERE FOR USER",user)
+  }
+  getUser()
+}, [])
 
+ //   if (user.empty) {
+    //   console.log('No matching documents.');
+    //   return;
+    // }
+
+    // user.forEach(doc => {
+    //   console.log(doc.id, '=>', doc.data());
+    // });
+
+
+  return (
     <View style={styles.container}>
-<Divider/>
+      <Divider />
       <View style={styles.contents}>
         <Text style={styles.sectionTitle}>Edit Profile</Text>
 
-          <View style={styles.form}>
-            <FlatList
-              ListHeaderComponent={firstItem}
-              ListFooterComponent={lastItem}
-              data={fields}
-              keyExtractor={(item) => item.id}
-              renderItem={({ item }) =>
+        <View style={styles.form}>
+          <FlatList
+            ListHeaderComponent={firstItem}
+            ListFooterComponent={lastItem}
+            data={fields}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
               <KeyboardAwareScrollView>
-                <Input labelStyle={{fontWeight: "normal",}} inputStyle={{color: "white", fontSize: 14}} label={item.field} />
-              </KeyboardAwareScrollView>}
-            />
-          </View>
+                <Input
+                  labelStyle={{ fontWeight: "normal" }}
+                  inputStyle={{ color: "white", fontSize: 14 }}
+                  label={item.field}
+                />
+              </KeyboardAwareScrollView>
+            )}
+          />
+        </View>
       </View>
-    <Footer />
+      <Footer />
     </View>
-
   );
 };
 
@@ -87,14 +121,14 @@ const styles = StyleSheet.create({
   },
   contents: {
     flex: 1.7,
-    alignItems: "center"
+    alignItems: "center",
   },
-  sectionTitle:{
+  sectionTitle: {
     fontSize: 16,
     fontWeight: "bold",
     color: "white",
     marginTop: 20,
-    marginBottom: 15
+    marginBottom: 15,
   },
   form: {
     alignSelf: "center",
